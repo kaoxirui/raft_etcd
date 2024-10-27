aad870a861d41353252262b690051241679d9f96
kaoxirui <1106231574@qq.com>
2024-10-27 21:43:29 +0800

    2024_10_27

--- raft-kv/raft/proto.cpp
+++ raft-kv/raft/proto.cpp
@@ -18,8 +18,47 @@ namespace proto {
             return 2;
         }
     }
-
-    uint32_t Entry::serialize_size() const {}
+    //1ULL:int 1 with type unsigned long long
+    //<< is bitshift operator. 1ULL<<i is equal to 2^i,or in binary:1000..00 with i zeros
+    //左移理解：二进制左移几位就是后面加几个0，前面去掉几位
+    //右移：在二进制前面加几位，正加0，负加1，后面去掉几位
+    //左移一位相当于乘以2，右移一位相当于除2（不完全等同）
+    static uint32_t u64_serialize_size(uint64_t d) {
+        if (d < (1ULL << 8)) {
+            if (d < (1ULL < 7)) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            if (d < (1ULL << 16)) {
+                return 3;
+            } else if (d < (1ULL << 32)) {
+                return 5;
+            } else {
+                return 9;
+            }
+        }
+    }
+    //计算数据长度为len的序列化大小
+    static uint32_t data_serialize_size(uint32_t len) {
+        if (len <= std::numeric_limits<uint8_t>::max()) {
+            //2^8-1
+            //2表示与序列后所需的额外的字节，其中一个存储len大小类型标志，另一个存储实际长度
+            return 2 + len;
+        }
+        if (len <= std::numeric_limits<uint16_t>::max()) {
+            return 3 + len;
+        }
+        if (len <= std::numeric_limits<uint32_t>::max()) {
+            return 5 + len;
+        }
+        assert(false);
+    }
+    uint32_t Entry::serialize_size() const {
+        return 1 + u8_serialize_size(type) + u64_serialize_size(term) + u64_serialize_size(index)
+               + data_serialize_size(static_cast<uint32_t>(data.size()));
+    }
 
     const char *msg_type_to_string(MessageType type) {
         switch (type) {
@@ -106,6 +145,21 @@ namespace proto {
     bool Message::is_response_msg() const {
         return type == MsgAppResp || type == MsgVoteResp || type == MsgHeartbeatResp
                || type == MsgUnreachable || type == MsgPreVoteResp;
+    }
+
+    /*
+    从data解析出一个ConfChange对象
+    通过 oh.get() 获取解包的 msgpack::object，然后使用 convert 方法将其内容转换并赋值给传入的 ConfChange &cc。
+    msgpack 会自动匹配并填充 cc 的字段（前提是 ConfChange 定义了 MSGPACK_DEFINE 宏）。
+    */
+    void ConfChange::from_data(const std::vector<uint8_t> &data, ConfChange &cc) {
+        msgpack::object_handle oh = msgpack::unpack((const char *)data.data(), data.size());
+        oh.get().convert(cc);
+    }
+    std::vector<uint8_t> ConfChange::serialize() const {
+        msgpack::sbuffer sbuf;
+        msgpack::pack(sbuf, *this);
+        return std::vector<uint8_t>(sbuf.data(), sbuf.data() + sbuf.size());
     }
 } // namespace proto
 } // namespace kv
--- raft-kv/raft/proto.h
+++ raft-kv/raft/proto.h
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <vector>
 #include <msgpack.hpp>
-#include "log.h"
+#include "../common/log.h"
 
 namespace kv {
 namespace proto {
@@ -64,8 +64,52 @@ namespace proto {
         uint64_t term;
         uint64_t index;
         std::vector<uint8_t> data;
+        /*
+        MSGPACK_DEFINE会自动生成这些成员变量的序列化和反序列化方法
+        MessagePack 的序列化过程会将 type、term、index、data 依次编码到一个
+         MessagePack 数据结构中，反序列化时会按顺序解码到相应的成员变量。
+         */
         MSGPACK_DEFINE(type, term, index, data);
     };
+    typedef std::shared_ptr<Entry> EntryPtr;
+
+    /// @brief 表示集群中的节点配置状态，保存和管理这些节点的信息，确保在配置更改时各节点状态是一致的
+    struct ConfState {
+        bool operator==(const ConfState &cs) const {
+            return nodes == cs.nodes && learners == cs.learners;
+        }
+        std::vector<uint64_t> nodes;
+        std::vector<uint64_t> learners;
+        MSGPACK_DEFINE(nodes, learners);
+    };
+    typedef std::shared_ptr<ConfState> ConfStatePtr;
+
+    //conf_state保存快照时集群配置状态，哪些是投票节点，哪些是学习者节点
+    //inedx快照中最后一条日志的索引
+    //term与index对应的任期编号
+    struct SnapshotMetadata {
+        SnapshotMetadata() : index(0), term(0) {}
+        bool operator==(const SnapshotMetadata &meta) const {
+            return index == meta.index && conf_state == meta.conf_state && term == meta.term;
+        }
+        ConfState conf_state;
+        uint64_t index;
+        uint64_t term;
+        MSGPACK_DEFINE(index, term);
+    };
+    struct Snapshot {
+        Snapshot() = default;
+        explicit Snapshot(const std::vector<uint8_t> &data) : data(data) {}
+        bool equal(const Snapshot &snap) const {
+            //如果operator==没有声明为const，不能比较const对象
+            return data == snap.data && metadata == snap.metadata;
+        }
+        bool is_empty() const { return metadata.index == 0; }
+        std::vector<uint8_t> data;
+        SnapshotMetadata metadata;
+        MSGPACK_DEFINE(data, metadata);
+    };
+    typedef std::shared_ptr<Snapshot> SnapshotPtr;
 
     struct Message {
         Message()
@@ -95,6 +139,39 @@ namespace proto {
                        context);
     };
     typedef std::shared_ptr<Message> MessagePtr;
+
+    /// @brief 记录了当前任期、投票对象和提交索引，通过持久化这些信息，可以在系统崩溃或重启后快速恢复
+    struct HardState {
+        HardState() : term(0), vote(0), commit(0) {}
+        bool is_empty_state() const { return term == 0 && vote == 0 && commit == 0; }
+        bool equal(const HardState &hs) const {
+            return term == hs.term && vote == hs.vote && commit == hs.commit;
+        }
+        uint64_t term;
+        uint64_t vote;
+        uint64_t commit;
+        MSGPACK_DEFINE(term, vote, commit);
+    };
+
+    const uint8_t ConfChangeAddNode = 0;
+    const uint8_t ConfChangeRemoveNode = 1;
+    const uint8_t ConfChangeUpdateNode = 2;
+    const uint8_t ConfChangeAddLearnerNode = 3;
+
+    struct ConfChange {
+        static void from_data(const std::vector<uint8_t> &data, ConfChange &cc);
+        std::vector<uint8_t> serialize() const;
+        //变更的唯一标志符
+        uint64_t id;
+        //配置变更的类型
+        uint8_t conf_change_type;
+        //受影响的节点id
+        uint64_t node_id;
+        //变更操作的上下文信息
+        std::vector<uint8_t> context;
+        MSGPACK_DEFINE(id, conf_change_type, node_id, context);
+    };
+    typedef std::shared_ptr<ConfChange> ConfChangePtr;
 
 } // namespace proto
 }; // namespace kv
