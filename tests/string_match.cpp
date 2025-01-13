36d5c6c40db1419ea13ab7dc91de7e1623e58788
kaoxirui <1106231574@qq.com>
2025-01-13 21:28:30 +0800

    test:单元测试

---
+++ tests/string_match.cpp
@@ -0,0 +1,33 @@
+#include "../raft-kv/server/redis_store.h"
+#include <gtest/gtest.h>
+#include <regex>
+using namespace kv;
+
+TEST(match, match) {
+    struct Test {
+        std::string pattern;
+        std::string key;
+        int match;
+    };
+
+    std::vector<Test> tests;
+    tests.push_back(Test{.pattern = "*", .key = "abc", .match = 1});
+    tests.push_back(Test{.pattern = "a*c", .key = "abc", .match = 1});
+    tests.push_back(Test{.pattern = "a*", .key = "abc", .match = 1});
+    tests.push_back(Test{.pattern = "b*", .key = "abc", .match = 0});
+    tests.push_back(Test{.pattern = "*c", .key = "abc", .match = 1});
+    tests.push_back(Test{.pattern = "*a", .key = "abc", .match = 0});
+    tests.push_back(Test{.pattern = "*b*", .key = "abc", .match = 1});
+    tests.push_back(Test{.pattern = "", .key = "abc", .match = 0});
+
+    for (Test &t : tests) {
+        int match =
+            string_match_len(t.pattern.data(), t.pattern.size(), t.key.data(), t.key.size(), 0);
+        ASSERT_TRUE(match == t.match);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
